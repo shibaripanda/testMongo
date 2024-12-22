a09e0d69fcc33ce754868f51b918e0bb92f8d217
Dima <prevetlunatikam@gmail.com>
2024-12-22 22:12:50 +0300

    ddf

--- DbClass.ts
+++ DbClass.ts
@@ -1,42 +1,48 @@
 import mongoose from "mongoose"
 import { appSchema } from "./model/App"
 
+interface MS {
+    model: string
+    scheme:  any
+}
+
+const modelsList = [{model: 'App', scheme: appSchema}]
+
 export class DbClass {
 
-    modelsList: any
+    modelsList: MS[]
+    mongoToken: string | undefined
 
     constructor(){
-        this.modelsList = {
-            'App': appSchema
-        }
+        this.modelsList = modelsList
+        this.mongoToken = process.env.MONGO_TOKEN
     }
 
     async mongoUse(dbName: string, modelName: string, func: any){
         return await func(await this.dbConnections(dbName, modelName))
     }
     async dbConnections(dbName: string, modelName: string){
-
         const db: any = mongoose.connection.useDb(dbName, { useCache: true })
         if(!db.model[modelName]){
-            db.model(modelName, this.modelsList[modelName])
+            db.model(modelName, this.modelsList.find(item => item.model === modelName)?.scheme)
         }
-        console.log('Db name: ', db.name, modelName)
+        console.log('DB name: ', db.name, modelName)
         return db.model(modelName)
-        
     }
     async dbMongoConect (){
-        let status = false
-        if(process.env.MONGO_TOKEN)
-        await mongoose.connect(process.env.MONGO_TOKEN)
-        .then((res) => {
-            console.log(`# connect to DB`)
-            status = true
-        })
-        .catch(async (error) => {
-            console.log('problem connecting to DB')
-            console.log(error)
-        })
-        return status
+        if(this.mongoToken){
+            await mongoose.connect(this.mongoToken)
+            .then(() => {
+                console.log(`# connect to DB`)
+            })
+            .catch(async (error) => {
+                console.log('# problem connecting to DB')
+                console.log(error)
+            })
+        }
+        else{
+            console.log('undefined mongo token')
+        }
     }
 
 }
